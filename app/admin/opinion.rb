4ddad78d975c90dccf9701852c3c32b5b7c1905a
Seb Ashton <seb@madebymade.co.uk>
2013-04-08 16:23:32 +0100

    activeadmin updates

--- app/admin/opinion.rb
+++ app/admin/opinion.rb
@@ -1,19 +1,21 @@
 if defined?(ActiveAdmin)
-  ActiveAdmin.register Comment::Opinion do
+  ActiveAdmin.register Comment::Opinion, {:sort_order => :created_at} do
     controller do
       cache_sweeper Comment.config.cache_sweeper if Comment.config.cache_sweeper
     end
 
-    filter :pubished
-    filter :start_at
-    filter :end_at
+    filter :name
+    filter :text
+    filter :published
 
     menu :label => 'Comments'
 
     index do
       column :name
       column :text
-      column :updated_at
+      column :created_at
+      column :published
+      column :thread
 
       default_actions
     end
