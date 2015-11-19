9a173b8eca4fe0dac32bb20d9e654564b3a10bd0
Seb Ashton <seb@sebashton.com>
2015-11-19 12:06:15 +0000

    Constrait engine routes to HTML

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Comment::Engine.routes.draw do
-  match '/:object/:object_id',                 :to => 'comment#thread', :as => 'comments_thread',      :via => [:get]
-  match '/:object/:object_id',                 :to => 'comment#new',    :as => 'comment_create',       :via => [:post]
+  match '/:object/:object_id', :to => 'comment#thread', :as => 'comments_thread', :via => [:get], :constraints => { :format => :html }
+  match '/:object/:object_id', :to => 'comment#new',    :as => 'comment_create',  :via => [:post], :constraints => { :format => :html }
 end
